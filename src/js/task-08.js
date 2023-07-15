9ebdaa43969d4fe531c5f6a4dee0486212aa9f1b
RomPshen <rom-ps@ukr.net>
2023-07-15 19:40:52 +0300

    add task_8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,25 +1,26 @@
 const form = document.querySelector(".login-form");
-const inputs = documents.querySelectorAll("input");
+const inputs = document.querySelectorAll("input");
 
 form.addEventListener("submit", onFormSubmit);
 
-inputs.foreach(input) => {
-    if (input.value.trim() === '') { 
-    alert("error")
-    }
-};
-
 function onFormSubmit(evt) {
     evt.preventDefault();
-
+    
     const formEls = evt.currentTarget.elements;
-
     const mail = formEls.email.value;
     const password = formEls.password.value;
+
+    inputs.forEach(input) => {
+        if (formEls.value === "") { 
+        alert("всі поля повинні бути заповнені")
+        };
+    };
 
     const formData = {
         mail,
         password,
     };
     console.log(formData);
+    document.getElementsByClassName(".login-form").reset();
 }
+
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
+const changeColorBtn = document.querySelector(".change-color");
+const colorNumb = document.querySelector(".color");
+
+changeColorBtn.addEventListener("click", onClick);
+
+function onClick() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorNumb.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
